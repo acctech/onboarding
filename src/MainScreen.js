2fc8969322583163de4853d77953f524f805f88f
Danny Falero <52477574+acctech@users.noreply.github.com>
2023-02-08 18:59:41 +1100

    using debug

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -12,18 +12,20 @@ export default function MainScreen(props) {
       "You appear to be offsite using a " +
       window.ui.os +
       " device. " +
-      (isAppleDevice ? "Redirecting..." : ""),
+      (isAppleDevice
+        ? "Redirecting..."
+        : "Your device is not supported for Offsite Setup. \nPlease wait until you are on campus to complete the setup process."),
     buttons: isAppleDevice
       ? []
       : [
-          {
-            label:
-              "Your device is not supported for Offsite Setup. \nPlease wait until you are on campus to complete the setup process.",
-            color: "#A88D5D",
-            textColor: "white",
-            icon: <EastIcon fontSize="small" sx={{ ml: 1 }}></EastIcon>,
-            action: () => {},
-          },
+          // {
+          //   label:
+          //     "Your device is not supported for Offsite Setup. \nPlease wait until you are on campus to complete the setup process.",
+          //   color: "#A88D5D",
+          //   textColor: "white",
+          //   icon: <EastIcon fontSize="small" sx={{ ml: 1 }}></EastIcon>,
+          //   action: () => {},
+          // },
         ],
   };
 
