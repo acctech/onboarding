3d4bd7e72b6570652d9ecba3c040ed28ab5dce5e
Danny Falero <52477574+acctech@users.noreply.github.com>
2023-02-08 19:19:52 +1100

    using debug

--- src/CoverPage.js
+++ src/CoverPage.js
@@ -153,7 +153,6 @@ export function CoverPage(props) {
         </Box>
       </Box>
       <Box
-        id={"bg"}
         zIndex={0}
         sx={{
           backgroundImage: "url(" + process.env.PUBLIC_URL + "/bg.jpg)",
@@ -167,6 +166,7 @@ export function CoverPage(props) {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
+          position: "absolute",
         }}
       ></Box>
       {debug && (
